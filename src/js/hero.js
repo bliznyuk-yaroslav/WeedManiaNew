81214e4350eaa9c6c49402c826c9540bddd931eb
Darya-Karnaukh <153089841+Darya-Karnaukh@users.noreply.github.com>
2025-01-19 13:36:14 +0200

    leaves mobile and desktop

---
+++ src/js/hero.js
@@ -0,0 +1,6 @@
+const buttonElemGoogle = document.querySelector('.js-button-googleplay');
+
+buttonElemGoogle.addEventListener('click', () => {
+  window.location.href =
+    'https://play.google.com/store/apps/details?id=com.sfgh.weedmania.gp';
+});
